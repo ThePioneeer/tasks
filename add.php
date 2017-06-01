b0819bad15268b9bb83e43322fd6fb7b0e529375
ThePioneeer <uko_1@hotmail.com>
2017-06-01 23:48:33 +0300

    Added validation class

--- add.php
+++ add.php
@@ -2,17 +2,29 @@
 include "core/init.php";
 
 if(Input::exists()){
-    $task = new Task();
-    try{
-        $task->create(array(
-                "name" => Input::get("name"),
-                "created_at" => date("Y-m-d H:i:s"),
-                "updated_at" => date("Y-m-d H:i:s")
-            ));
+    $validate = new Validate();
+    $validation = $validate->check($_POST, array(
+        "name" => array(
+            "required" => true,
+            "max" => 255
+        )
+    ));
 
-        Redirect::to("index.php");
-    }catch(Exception $e){
-        echo $e->getMessage();
+    if($validation->passed()){
+        $task = new Task();
+        try{
+            $task->create(array(
+                    "name" => Input::get("name"),
+                    "created_at" => date("Y-m-d H:i:s"),
+                    "updated_at" => date("Y-m-d H:i:s")
+                ));
+
+            Redirect::to("index.php");
+        }catch(Exception $e){
+            die($e->getMessage());
+        }
     }
     
 }
+
+Redirect::to("index.php");
